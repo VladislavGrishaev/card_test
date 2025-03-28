03ef0bb1f4b4b66a83f65b24e92fafcde2882c1a
VladislavG <grishvm@yandex.ru>
2025-03-28 10:04:40 +0500

    фикс анимации раскрытия, считывание контента через JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-  (function (){
-    const showContentBtn = document.querySelector('.card__show-more-btn');
-    const contentWrap = document.querySelector('.card__content');
+document.addEventListener("DOMContentLoaded", function() {
+  function toggleTextContent(contentWrapper, textContent, toggleButton) {
+    if (!textContent || !toggleButton || !contentWrapper) return;
 
-    showContentBtn.addEventListener('click', () => {
-      contentWrap.classList.toggle('active-full-content');
+    const stylesContent = window.getComputedStyle(textContent);
+    const parseLineHeight = parseFloat(stylesContent.lineHeight);
+    const collapsedHeight = parseLineHeight * 5;
+
+    textContent.style.maxHeight = collapsedHeight + "px";
+
+    let expanded = false;
+
+    toggleButton.addEventListener("click", function() {
+      if (!expanded) {
+        textContent.style.maxHeight = textContent.scrollHeight + "px";
+        contentWrapper.classList.add('active-full-content');
+      } else {
+        textContent.style.maxHeight = collapsedHeight + "px";
+        contentWrapper.classList.remove('active-full-content');
+      }
+      expanded = !expanded;
     });
-  })();
+  }
+
+  const textBlock = document.querySelector(".card__text-content");
+  const button = document.querySelector(".card__show-more-btn");
+  const contentWrap = document.querySelector(".card__content");
+
+  toggleTextContent(contentWrap, textBlock, button);
 });
